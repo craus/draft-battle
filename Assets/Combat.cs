4a33ec58353d956c30e5fd6b02317dfbbdfceeca
craus <craus902@gmail.com>
2017-01-03 14:08:39 +0400

    enemies, start spawn points

--- Assets/Combat.cs
+++ Assets/Combat.cs
@@ -4,6 +4,7 @@ using UnityEngine.Networking;
 public class Combat : NetworkBehaviour
 {
     public const int maxHealth = 100;
+    public bool destroyOnDeath;
 
     [SyncVar]
     public int health = maxHealth;
@@ -14,10 +15,14 @@ public class Combat : NetworkBehaviour
 
         health -= amount;
         if (health <= 0) {
-            health = maxHealth;
+            if (destroyOnDeath) {
+                Destroy(gameObject);
+            } else {
+                health = maxHealth;
 
-            // called on the server, will be invoked on the clients
-            RpcRespawn();
+                // called on the server, will be invoked on the clients
+                RpcRespawn();
+            }
         }
     }
 
