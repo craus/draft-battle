0dfb299a9ddc100101ad581669fdd0d0cef1f7b6
craus <craus902@gmail.com>
2017-01-03 13:57:18 +0400

    health, hit, respawn

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -6,6 +6,9 @@ public class Bullet : MonoBehaviour
         var hit = collision.gameObject;
         var hitPlayer = hit.GetComponent<PlayerMove>();
         if (hitPlayer != null) {
+            var combat = hit.GetComponent<Combat>();
+            combat.TakeDamage(10);
+
             Destroy(gameObject);
         }
     }
