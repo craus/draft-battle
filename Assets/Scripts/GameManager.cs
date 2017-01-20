0c88302f90b4467874a8dd44329e3cfad6a9a1a4
Pavel Semushin <pavel.semushin@castle.co>
2017-01-20 13:50:12 +0400

    gameManager, console

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+
+public class GameManager : NetworkBehaviour
+{
+    public const int maxHealth = 100;
+    public bool destroyOnDeath;
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            RpcMessage(42);
+        }
+    }
+
+    private IEnumerator Start() {
+        Console.instance.Log(string.Format("isServer = {0}", isServer));
+        if (!isServer) {
+            yield break;
+        }
+        while (true) {
+            yield return new WaitForSecondsRealtime(5);
+            RpcMessage(UnityEngine.Random.Range(0,100000));
+        }
+    }
+
+    [ClientRpc]
+    void RpcMessage(int x) {
+        Console.instance.Log("M" + isServer + x);
+    }
+}
---
+++ Assets/Scripts/UI/Console.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Console : MonoBehaviour
+{
+    public static Console instance;
+    Text text;
+
+    public void Awake() {
+        instance = this;
+        text = GetComponent<Text>();
+    }
+
+    public void Log(string message) {
+        Debug.LogFormat(message);
+        text.text += message + "\n";
+    }
+
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            Log("T");
+        }
+        if (Input.GetKeyDown(KeyCode.A)) {
+            Console.instance.Log("A" + Network.isServer + Network.isClient);
+        }
+    }
+}
